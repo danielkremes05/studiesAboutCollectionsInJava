86ee6f25f34719e8c91922fc8ae3947b33345d1e
danielkremes05 <danielkremes05@gmail.com>
2024-06-18 14:15:08 -0300

    Studies About List Done

--- src/collections/lists/sortingLists/sortingPeople/SortingPeople.java
+++ src/collections/lists/sortingLists/sortingPeople/SortingPeople.java
@@ -1,4 +1,29 @@
 package collections.lists.sortingLists.sortingPeople;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class SortingPeople {
+
+    private List<People> people = new ArrayList<>();
+
+    public void addPerson (int agePeople, String namePeople, double heightPeople) {
+        people.add(new People(agePeople, namePeople, heightPeople));
+    }
+
+    public void sortByAge () {
+        Collections.sort(people);
+        for (People value : people) {
+            System.out.println(value);
+        }
+    }
+
+    public void sortByHeight () {
+        people.sort(Comparator.comparingDouble(People::getHeight));
+        for (People value : people) {
+            System.out.println(value);
+        }
+    }
 }
